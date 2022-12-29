1f92470bc4bf171f70141086838faafbc2944870
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-12-29 10:00:06 -0500

    Implement POST /questions with GET redirect (#7)
    * Configure launch command, rename handler
    * Add another header

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'];
 
-if ($method === 'GET' && preg_match('/^\/questions\/(\d+)$/', $path, $matches)) {
+if (preg_match('/^\/questions\/(\d+)$/', $path, $matches)) {
   // The path matches the route, so retrieve the number from the URL
   $number = intval($matches[1]);
   $filePath = $_SERVER['DOCUMENT_ROOT'] . "\api\question.php";
@@ -14,7 +14,7 @@ if ($method === 'GET' && preg_match('/^\/questions\/(\d+)$/', $path, $matches)) 
     include $filePath;
     exit();
   }
-} else if ($method === 'GET' && preg_match('/^\/([^\/]+)/', $path, $matches)) {
+} else if (preg_match('/^\/([^\/]+)/', $path, $matches)) {
   // Extract the name of the file from the route
   $name = $matches[1];
 
@@ -29,5 +29,20 @@ if ($method === 'GET' && preg_match('/^\/questions\/(\d+)$/', $path, $matches)) 
 
 // Return a 404 error for any other paths or methods
 http_response_code(404);
-echo 'Not Found';
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Borum Forum API</title>
+</head>
+
+<body>
+<h1>Borum Forum REST API</h1>
+</body>
+
+</html>
