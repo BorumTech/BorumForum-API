f8b1fe7f7f79bd54cf1d2dd1b09f3f6160f592ad
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-12-29 02:54:03 -0500

    Add another header

--- api/test.php
+++ api/test.php
@@ -2,6 +2,7 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(303);
+    header("HTTP/1.1 303 See Other");
     header("Location: {$_SERVER['REQUEST_URI']}");
     exit();
 }
